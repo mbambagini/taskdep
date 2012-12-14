27870f4e64af0d1076cd6367fc1fd44e16fcda01
mbambagini <mario.bambagini@gmail.com>
2012-12-14 12:23:37 +0100

    OK, MB

--- script.sh
+++ script.sh
@@ -1,29 +1,37 @@
 #! /bin/sh
 
 
-#### PARAMETERS
+################################## PARAMETERS ##################################
 
-#GENERATOR
-#0: SOFTWARE LIKE GENERATOR
+########## GENERATOR
+#0: CODE-STYLE GENERATOR
 #1: DATA FLOW GENERATOR
 GEN=0
 
-#WCET
+########## WCET: WORST-CASE EXECUTION TIME
 maxWcet=5000
 minWcet=1000
 
-#EXCHANGED DATA
+########## AMOUNT OF EXCHANGED DATA
 maxData=200
 minData=100
 
-#GENERATION
+########## TASK NUMBER
 tasks=30
-deps=15
-distance=5
+
+########## IF GEN==0
 #prob=0.0 #fully sequential
 #prob=1.0 #fully parallel
 prob=0.5
+##########
 
+########## USEFUL IF GEN==1
+deps=15
+distance=5
+##########
+
+
+################################## PROCESSING ##################################
 
 #### ARGUMENT
 if [ $# -lt 1 ];
